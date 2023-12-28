a1c59d979280345264b0bf9ab3cae07c409cb2e4
ebrahim-sani <ibraheemsani035@gmail.com>
2023-12-28 08:29:15 +0100

    rafactored get-student route..

--- app/api/get-student/route.ts
+++ app/api/get-student/route.ts
@@ -14,11 +14,13 @@ export async function GET(req: Request) {
       const student = await prisma.student.findUnique({
          where: { id: session?.user.id },
       });
-      if (student) {
-         return new Response(JSON.stringify(student), { status: 200 });
+      if (!student) {
+         return new Response("", { status: 404 });
       }
+      return new Response(JSON.stringify(student), { status: 200 });
    } catch (e) {
       console.log("Error fetching student", e);
+      return new Response("", { status: 500 });
    } finally {
       await prisma.$disconnect();
    }
